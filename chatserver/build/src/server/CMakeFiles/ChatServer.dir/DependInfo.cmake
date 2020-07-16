a2ab248e21307ab5eae40541a93949ab8c7d1175
sunwenbin123 <59340649+sunwenbin123@users.noreply.github.com>
2020-07-16 18:11:40 +0800

    Add files via upload

---
+++ chatserver/build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sunwenbin/Vscode_project/chatserver/src/server/chatserver.cpp" "/home/sunwenbin/Vscode_project/chatserver/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/sunwenbin/Vscode_project/chatserver/src/server/chatservice.cpp" "/home/sunwenbin/Vscode_project/chatserver/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/sunwenbin/Vscode_project/chatserver/src/server/db/db.cpp" "/home/sunwenbin/Vscode_project/chatserver/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/sunwenbin/Vscode_project/chatserver/src/server/main.cpp" "/home/sunwenbin/Vscode_project/chatserver/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/sunwenbin/Vscode_project/chatserver/src/server/model/friendmoel.cpp" "/home/sunwenbin/Vscode_project/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/friendmoel.cpp.o"
+  "/home/sunwenbin/Vscode_project/chatserver/src/server/model/groupmodel.cpp" "/home/sunwenbin/Vscode_project/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/sunwenbin/Vscode_project/chatserver/src/server/model/offlinemessagemodel.cpp" "/home/sunwenbin/Vscode_project/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/sunwenbin/Vscode_project/chatserver/src/server/model/usermodel.cpp" "/home/sunwenbin/Vscode_project/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/sunwenbin/Vscode_project/chatserver/src/server/redis/redis.cpp" "/home/sunwenbin/Vscode_project/chatserver/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
